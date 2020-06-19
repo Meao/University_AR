67251b6ba11a9752fdd7bf33f70b75262f58f959
Marina Krivtsoun <marinaktsn@gmail.com>
2020-06-19 18:30:34 +0300

    first commit

---
+++ Assets/Scripts/Gaze.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour
+{
+    List<infoBehavior> infos = new List<infoBehavior>();
+
+    void Start()
+    {
+        infos = FindObjectsOfType<infoBehavior>().ToList();
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position,transform.forward, out RaycastHit hit))
+        {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("hasInfo"))
+            {
+                OpenInfo(go.GetComponent<infoBehavior>());
+            }
+        } else
+        {
+            CloseAll();
+        }
+    }
+
+    void OpenInfo(infoBehavior desiredInfo)
+    {
+        foreach (infoBehavior info in infos)
+        {
+            if (info == desiredInfo)
+            {
+                info.OpenInfo();
+            } else
+            {
+                info.CloseInfo();
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (infoBehavior info in infos)
+        {
+            info.CloseInfo();
+        }
+    }
+}
